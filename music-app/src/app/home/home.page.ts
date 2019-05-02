de3613f735fbdc2ba4ba7aeef94aa505f30264cd
Edwin Cruz <edwincruz25@unitec.edu>
2019-05-02 00:26:32 -0600

    added .env

---
+++ music-app/src/app/home/home.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var cordova: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
+})
+export class HomePage implements OnInit {
+  result = {};
+  constructor() {}
+
+  ngOnInit() {}
+
+  authWithSpotify() {
+    console.log('entre a la autenticacion');
+    const config = {
+      clientId: '5c3b13a967bc4bf598898a7eaac6e54a ',
+      redirectUrl: 'spotify-app-ec://callback',
+      scopes: [
+        'streaming',
+        'playlist-read-private',
+        'user-read-email',
+        'user-read-private'
+      ],
+      tokenExchangeUrl: 'https://spotify-app-ec.herokuapp.com/exchange',
+      tokenRefreshUrl: 'https://spotify-app-ec.herokuapp.com/refresh'
+    };
+    console.log({ config });
+
+    cordova.plugins.spotifyAuth
+      .authorize(config)
+      .then(({ accessToken, encryptedRefreshToken, expiresAt }) => {
+        console.log('entre a la autenticacion');
+
+        console.log(accessToken, encryptedRefreshToken, expiresAt);
+        this.result = {
+          access_token: accessToken,
+          expires_in: expiresAt,
+          ref: encryptedRefreshToken
+        };
+      })
+      .catch(e => {
+        console.log('Erroooor');
+        console.log(e);
+      });
+  }
+}
---
+++ music-app/src/app/songs/songs.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-songs',
+  templateUrl: './songs.page.html',
+  styleUrls: ['./songs.page.scss']
+})
+export class SongsPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
