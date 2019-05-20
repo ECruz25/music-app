2acf519de479f18f5051fd0cc32c8ba1cb5fdafc
Edwin Cruz <edwincruz25@unitec.edu>
2019-05-20 02:16:02 -0600

    vamonos

--- music-app/src/app/home/home.page.ts
+++ music-app/src/app/home/home.page.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 declare var cordova: any;
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.page.html",
-  styleUrls: ["./home.page.scss"]
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
   result = {};
@@ -14,25 +14,25 @@ export class HomePage implements OnInit {
   ngOnInit() {}
 
   authWithSpotify() {
-    console.log("entre a la autenticacion");
+    console.log('entre a la autenticacion');
     const config = {
-      clientId: "5c3b13a967bc4bf598898a7eaac6e54a ",
-      redirectUrl: "spotify-app-ec://callback",
+      clientId: '5c3b13a967bc4bf598898a7eaac6e54a ',
+      redirectUrl: 'spotify-app-ec://callback',
       scopes: [
-        "streaming",
-        "playlist-read-private",
-        "user-read-email",
-        "user-read-private"
+        'streaming',
+        'playlist-read-private',
+        'user-read-email',
+        'user-read-private'
       ],
-      tokenExchangeUrl: "https://spotify-app-ec.herokuapp.com/exchange",
-      tokenRefreshUrl: "https://spotify-app-ec.herokuapp.com/refresh"
+      tokenExchangeUrl: 'https://spotify-app-ec.herokuapp.com/exchange',
+      tokenRefreshUrl: 'https://spotify-app-ec.herokuapp.com/refresh'
     };
     console.log({ config });
 
     cordova.plugins.spotifyAuth
       .authorize(config)
       .then(({ accessToken, encryptedRefreshToken, expiresAt }) => {
-        console.log("entre a la autenticacion");
+        console.log('entre a la autenticacion');
 
         console.log(accessToken, encryptedRefreshToken, expiresAt);
         this.result = {
@@ -42,7 +42,7 @@ export class HomePage implements OnInit {
         };
       })
       .catch(e => {
-        console.log("Erroooor");
+        console.log('Erroooor');
         console.log(e);
       });
   }
