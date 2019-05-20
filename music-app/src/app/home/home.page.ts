d08fa2e46500547f57fd71d237fa5866a2fad6ac
Edwin Cruz <edwincruz25@unitec.edu>
2019-05-20 02:35:10 -0600

    asd

--- music-app/src/app/home/home.page.ts
+++ music-app/src/app/home/home.page.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 declare var cordova: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"]
 })
 export class HomePage implements OnInit {
   result = {};
@@ -14,35 +14,35 @@ export class HomePage implements OnInit {
   ngOnInit() {}
 
   authWithSpotify() {
-    console.log('entre a la autenticacion');
+    console.log("entre a la autenticacion");
     const config = {
-      clientId: '5c3b13a967bc4bf598898a7eaac6e54a ',
-      redirectUrl: 'spotify-app-ec://callback',
+      clientId: "5c3b13a967bc4bf598898a7eaac6e54a",
+      redirectUrl: "spotify-app-ec://callback",
       scopes: [
-        'streaming',
-        'playlist-read-private',
-        'user-read-email',
-        'user-read-private'
+        "streaming",
+        "playlist-read-private",
+        "user-read-email",
+        "user-read-private"
       ],
-      tokenExchangeUrl: 'https://spotify-app-ec.herokuapp.com/exchange',
-      tokenRefreshUrl: 'https://spotify-app-ec.herokuapp.com/refresh'
+      tokenExchangeUrl: "https://spotify-app-ec.herokuapp.com/exchange",
+      tokenRefreshUrl: "https://spotify-app-ec.herokuapp.com/refresh"
     };
     console.log({ config });
 
     cordova.plugins.spotifyAuth
       .authorize(config)
-      .then(({ accessToken, encryptedRefreshToken, expiresAt }) => {
-        console.log('entre a la autenticacion');
+      .then(response => {
+        console.log("entre a la autenticacion");
 
-        console.log(accessToken, encryptedRefreshToken, expiresAt);
+        console.log(response);
         this.result = {
-          access_token: accessToken,
-          expires_in: expiresAt,
-          ref: encryptedRefreshToken
+          access_token: response.accessToken,
+          expires_in: response.expiresAt,
+          ref: response.encryptedRefreshToken
         };
       })
       .catch(e => {
-        console.log('Erroooor');
+        console.log("Erroooor");
         console.log(e);
       });
   }
